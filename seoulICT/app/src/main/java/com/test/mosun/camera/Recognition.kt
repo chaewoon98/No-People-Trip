eb299e3a635c54ec3c63a9a3be88fe962bf2ec8d
mo_ah <mo_ah@naver.com>
2020-08-23 16:19:31 +0900

    remove file

--- seoulICT/app/src/main/java/com/test/mosun/camera/Recognition.kt
+++
@@ -1,9 +0,0 @@
-package com.radslow.tflitedemo
-
-data class Recognition(
-    val name: String,
-    val probability: Float
-) {
-    override fun toString() =
-        "$name : ${probability*100}%"
-}
